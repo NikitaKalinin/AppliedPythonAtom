427b39c6bc7ce982e05af58ca318a437b63741fa
nikita <quilpboy@inbox.ru>
2019-03-07 04:10:08 +0300

    HW1

--- homeworks/homework_01/hw1_invertint.py
+++ homeworks/homework_01/hw1_invertint.py
@@ -9,4 +9,17 @@ def reverse(number):
     :param number: исходное число
     :return: инвертированное число
     '''
+    number = str(number)
+    number = number[::-1]
+    flag = False
+    if number[len(number)-1] == "-":
+        number = number[len(number)-1] + number[0:len(number)-1]
+        flag = True
+    if flag:
+        while number[1] == 0 and len(number) >= 2:
+            number = number[0] + number[2:]
+    else:
+        while number[0] == 0 and len(number) >= 1:
+            number = number[1:]
+    return int(number)
     raise NotImplementedError
