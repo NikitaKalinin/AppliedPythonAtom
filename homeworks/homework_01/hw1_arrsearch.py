dddd00a977229bdffcff1bf75199115e9da25f27
nikita <quilpboy@inbox.ru>
2019-03-07 04:07:15 +0300

    HW1

--- homeworks/homework_01/hw1_arrsearch.py
+++ homeworks/homework_01/hw1_arrsearch.py
@@ -13,4 +13,11 @@ def find_indices(input_list, n):
     :param n: целевая сумма
     :return: tuple из двух индексов или None
     '''
+    a = input_list
+    for i in range(len(a)):
+        if (n-a[i]) in a:
+            if a[i] != n-a[i]:
+                out = (i, a.index(n-a[i]))
+                return out
+    return None
     raise NotImplementedError
--- homeworks/homework_01/hw1_calcadv.py
+++ homeworks/homework_01/hw1_calcadv.py
@@ -2,6 +2,21 @@
 # coding: utf-8
 
 
+def check_opr(x):
+    if x is "+" or x is "-" or x is "*" or x is "/" or x is "(" or x is ")":
+        return True
+    else:
+        return False
+
+
+def check_num(x):
+    try:
+        k = int(x)
+        return True
+    except:
+        return False
+
+
 def advanced_calculator(input_string):
     '''
     Калькулятор на основе обратной польской записи.
@@ -9,5 +24,37 @@ def advanced_calculator(input_string):
      плюс, минус, умножить, делить
     :param input_string: строка, содержащая выражение
     :return: результат выполнение операции, если строка валидная - иначе None
+    l = input_string
+    l = l.replace(" ", "")
+    k = 1
+    out_l, opr_l = [], []
+    if l is None:
+        return None
+    for i in range(len(l)):
+        k *= int(check_opr(l[i]) or check_num(l[i]))
+    if not bool(k):
+        return None
+    while "--" in l or "++" in l or "+-" in l or "-+" in l:
+        l = l.replace("--", "+")
+        l = l.replace("++", "+")
+        l = l.replace("+-", "-")
+        l = l.replace("-+", "-")
+    if "**" in l or "//" in l or "*/" in l or "/*" in l:
+        return None
+    if "*-" in l or "-*" in l or "*+" in l or "+*" in l:
+        return None
+    if "/-" in l or "-/" in l or "/+" in l or "+/" in l:
+        return None
+    while l != "":
+        s = ""
+        while  l != "" and check_num(l[0]):
+            s = s + l[0]
+            if len(l) > 1:
+                l = l[1:]
+            else:
+                l = ""
+        if s != "":
+            out_l.append(int(s))
+        s = ""
     '''
     raise NotImplementedError
