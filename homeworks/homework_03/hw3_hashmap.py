309cc0726b47f1e27de7890f8aa5448493f0f90b
nikita <quilpboy@inbox.ru>
2019-03-18 03:32:27 +0300

    HashMap done

--- homeworks/homework_03/hw3_hashmap.py
+++ homeworks/homework_03/hw3_hashmap.py
@@ -2,6 +2,9 @@
 # coding: utf-8
 
 
+import itertools
+
+
 class HashMap:
     '''
     Давайте сделаем все объектненько,
@@ -14,69 +17,114 @@ class HashMap:
             :param key: ключ
             :param value: значение
             '''
+            self.key = key
+            self.value = value
 
         def get_key(self):
             # TODO возвращаем ключ
-            raise NotImplementedError
+            return self.key
 
         def get_value(self):
             # TODO возвращаем значение
-            raise NotImplementedError
+            return self.value
 
         def __eq__(self, other):
             # TODO реализовать функцию сравнения
-            raise NotImplementedError
+            return self.key == other.get_key()
 
     def __init__(self, bucket_num=64):
         '''
         Реализуем метод цепочек
         :param bucket_num: число бакетов при инициализации
         '''
-        raise NotImplementedError
+        self.buckets = [[] for i in range(bucket_num)]
+        self.capacity = 0
 
     def get(self, key, default_value=None):
         # TODO метод get, возвращающий значение,
         #  если оно присутствует, иначе default_value
-        raise NotImplementedError
+        if key in self:
+            index = self._get_index(self._get_hash(key))
+            for entry in self.buckets[index]:
+                if entry.key == key:
+                    return entry.value
+        else:
+            return default_value
 
     def put(self, key, value):
         # TODO метод put, кладет значение по ключу,
         #  в случае, если ключ уже присутствует он его заменяет
-        raise NotImplementedError
+        try:
+            h = hash(key)
+        except TypeError:
+            return
+        index = self._get_index(hash(key))
+        if key in self:
+            for entry in self.buckets[index]:
+                if entry.key == key:
+                    entry.value = value
+        else:
+            self.buckets[index].append(self.Entry(key, value))
+            self.capacity += 1
+            if (self.capacity/len(self.buckets)) > (2/3):
+                self._resize()
 
     def __len__(self):
         # TODO Возвращает количество Entry в массиве
-        raise NotImplementedError
+        return self.capacity
 
     def _get_hash(self, key):
         # TODO Вернуть хеш от ключа,
         #  по которому он кладется в бакет
-        raise NotImplementedError
+        return hash(key)
 
     def _get_index(self, hash_value):
         # TODO По значению хеша вернуть индекс элемента в массиве
-        raise NotImplementedError
+        return hash_value % len(self.buckets)
 
     def values(self):
         # TODO Должен возвращать итератор значений
-        raise NotImplementedError
+        it = 0
+        for entries in self.buckets:
+            if it == 0:
+                it = iter([entry.value for entry in entries])
+            else:
+                it = itertools.chain(it, iter([entry.value for entry in entries]))
+        return it
 
     def keys(self):
         # TODO Должен возвращать итератор ключей
-        raise NotImplementedError
+        it = 0
+        for entries in self.buckets:
+            if it == 0:
+                it = iter([entry.key for entry in entries])
+            else:
+                it = itertools.chain(it, iter([entry.key for entry in entries]))
+        return it
 
     def items(self):
         # TODO Должен возвращать итератор пар ключ и значение (tuples)
-        raise NotImplementedError
+        return zip(self.keys(), self.values())
 
     def _resize(self):
         # TODO Время от времени нужно ресайзить нашу хешмапу
-        raise NotImplementedError
+        new_buckets = [[] for i in range(len(self.buckets) * 2)]
+        for entries in self.buckets:
+            for entry in entries:
+                index = hash(entry.key) % len(new_buckets)
+                new_buckets[index].append(entry)
+        self.buckets = new_buckets
 
     def __str__(self):
         # TODO Метод выводит "buckets: {}, items: {}"
-        raise NotImplementedError
+        buckets = ",".join(str(i) for i in self.buckets)
+        items = ", ".join(str(i) for i in self.items())
+        return 'buckets: {' + buckets + '}, items: {' + items + '}'
 
     def __contains__(self, item):
         # TODO Метод проверяющий есть ли объект (через in)
-        raise NotImplementedError
+        for bucket in self.buckets:
+            for entry in bucket:
+                if entry.key == item:
+                    return True
+        return False
