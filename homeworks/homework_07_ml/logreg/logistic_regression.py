466d6413a3cbfb87e4087ddafdbf18ff0d5e0c83
nikita <quilpboy@inbox.ru>
2019-04-23 05:53:45 +0300

    without kaggle

--- homeworks/homework_07_ml/logreg/logistic_regression.py
+++ homeworks/homework_07_ml/logreg/logistic_regression.py
@@ -3,17 +3,24 @@
 
 
 import numpy as np
-
+from sklearn.preprocessing import LabelBinarizer, StandardScaler
+import math
 
 class LogisticRegression:
-    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5):
+    def __init__(self, lambda_coef=1.0, regulatization=None, alpha=0.5, n_iter = 1000):
         """
         LogReg for Binary case
         :param lambda_coef: constant coef for gradient descent step
         :param regulatization: regularizarion type ("L1" or "L2") or None
         :param alpha: regularizarion coefficent
         """
-        pass
+        self.cof = lambda_coef
+        self.type = regulatization
+        self.alpha = alpha
+        self.weights = None
+        self.labels = None
+        self.iterat = n_iter
+        self.scaler = StandardScaler()
 
     def fit(self, X_train, y_train):
         """
@@ -22,7 +29,32 @@ class LogisticRegression:
         :param y_train: target values for training data
         :return: None
         """
-        pass
+        lb = LabelBinarizer()
+        train_y = lb.fit(y_train).transform(y_train)
+        if train_y.shape[1] == 1:
+            train_y = np.append(1-train_y, train_y, axis=1)
+        self.labels = lb.classes_
+        self.weights = np.zeros((train_y.shape[1], X_train.shape[1] + 1))
+        self.scaler.fit(X_train)
+        train_x = self.scaler.transform(X_train)
+        train_x = np.hstack((np.ones((train_x.shape[0], 1)), train_x))
+        for _ in range(self.iterat):
+            for i in range(train_y.shape[0]):
+                deriative = np.zeros((train_y.shape[1], X_train.shape[1] + 1))
+                for j in range(train_y.shape[1]):
+                    a = -np.dot(train_x[i], self.weights[j])
+                    try:
+                        deriative[j] += train_x[i] * (1 / (1 + math.exp(a)) - train_y[i, j])
+                    except OverflowError:
+                        deriative[j] -= train_x[i] * train_y[i, j]
+                    if self.type is not None:
+                        if self.type == 'L1':
+                            deriative[j] += self.alpha * np.sign(self.weights[j])
+                        elif self.type == 'L2':
+                            deriative[j] += self.alpha * self.weights[j]
+                        else:
+                            raise ValueError
+                self.weights -= self.cof * deriative
 
     def predict(self, X_test):
         """
@@ -30,7 +62,10 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted values
         """
-        pass
+        if self.weights is not None:
+            return [self.labels[i] for i in np.argmax(self.predict_proba(X_test), axis=1)]
+        else:
+            raise ValueError
 
     def predict_proba(self, X_test):
         """
@@ -38,11 +73,31 @@ class LogisticRegression:
         :param X_test: test data for predict in
         :return: y_test: predicted probabilities
         """
-        pass
+        if self.weights is not None:
+            out, into = [], []
+            test_X = self.scaler.transform(X_test)
+            test_X = np.hstack((np.ones((test_X.shape[0], 1)), test_X))
+            for row in test_X:
+                into = []
+                for wths in self.weights:
+                    try:
+                        a = math.exp(-np.dot(row, wths))
+                        into.append(1 / (1 + a))
+                    except OverflowError:
+                        into.append(0)
+                if np.sum(into) != 0:
+                    into = into / np.sum(into)
+                out.append(list(into))
+            return np.array(out)
+        else:
+            raise ValueError
 
     def get_weights(self):
         """
         Get weights from fitted linear model
         :return: weights array
         """
-        pass
+        if self.weights is not None:
+            return self.weights
+        else:
+            raise ValueError
--- homeworks/homework_07_ml/metrics/metrics.py
+++ homeworks/homework_07_ml/metrics/metrics.py
@@ -3,16 +3,27 @@
 
 
 import numpy as np
+from sklearn.preprocessing import LabelBinarizer
 
 
-def logloss(y_true, y_pred):
+def logloss(y_true, y_pred, eps=0.0001):
     """
     logloss
     :param y_true: vector of truth (correct) class values
     :param y_hat: vector of estimated probabilities
     :return: loss
     """
-    pass
+    lb = LabelBinarizer()
+    labels = lb.fit(y_true).transform(y_true)
+    preds = np.clip(y_pred, eps, 1-eps)
+    if preds.ndim == 1:
+        preds = preds[:, np.newaxis]
+    if preds.shape[1] == 1:
+        preds = np.append(1 - preds, preds, axis=1)
+    if labels.shape[1] == 1:
+        labels = np.append(1-labels, labels, axis=1)
+    preds /= np.sum(preds, axis=1)[:, np.newaxis]
+    return -np.sum(labels * np.log(preds))/preds.shape[0]
 
 
 def accuracy(y_true, y_pred):
@@ -22,7 +33,7 @@ def accuracy(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    return np.sum(np.array(y_true) == np.array(y_pred))/len(y_true)
 
 
 def presicion(y_true, y_pred):
@@ -32,7 +43,11 @@ def presicion(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    true_y, pred_y = np.array(y_true), np.array(y_pred)
+    if np.sum(pred_y) == 0:
+        return 0.0
+    else:
+        return np.sum(true_y[pred_y == 1]) / np.sum(pred_y)
 
 
 def recall(y_true, y_pred):
@@ -42,7 +57,11 @@ def recall(y_true, y_pred):
     :param y_hat: vector of estimated class values
     :return: loss
     """
-    pass
+    true_y, pred_y = np.array(y_true), np.array(y_pred)
+    if np.sum(true_y) == 0:
+        return 0.0
+    else:
+        return np.sum(pred_y[true_y == 1]) / np.sum(true_y)
 
 
 def roc_auc(y_true, y_pred):
@@ -52,4 +71,31 @@ def roc_auc(y_true, y_pred):
     :param y_hat: vector of estimated probabilities
     :return: loss
     """
-    pass
+    true_y, pred_y = np.array(y_true), np.array(y_pred)
+    by_pred = np.argsort(pred_y)
+    true_y, pred_y = np.array([true_y[i] for i in by_pred]), np.array([pred_y[i] for i in by_pred])
+    q = len(true_y)
+    out = 0
+    out_devis = 0
+    for i in range(q):
+        for j in range(q):
+            devis = value_f(true_y[i], true_y[j])
+            out += preds_f(pred_y[i], pred_y[j]) * devis
+            out_devis += devis
+    return out/out_devis if out_devis != 0 else 0
+
+
+def preds_f(a1, a2):
+    if a1 > a2:
+        return 0
+    elif a1 == a2:
+        return 0.5
+    else:
+        return 1
+
+
+def value_f(a1, a2):
+    if a1 >= a2:
+        return 0
+    else:
+        return 1
